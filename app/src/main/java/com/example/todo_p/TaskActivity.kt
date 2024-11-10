1903e7ce80cf7b5a464e045e4ac77192eefe5589
PARTHA SARATHI MANNA <mannapartha2004@gmail.com>
2024-11-10 21:38:55 +0530

    add viewBinding

--- app/src/main/java/com/example/todo_p/TaskActivity.kt
+++ app/src/main/java/com/example/todo_p/TaskActivity.kt
@@ -8,9 +8,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.room.Room
 import java.util.Calendar
+import com.example.todo_p.databinding.ActivityTaskBinding
 
 const val DB_NAME = "todo.db"
 class TaskActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityTaskBinding
 
     lateinit var myCalender: Calendar
 
@@ -26,11 +29,13 @@ class TaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_task)
+        binding = ActivityTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }

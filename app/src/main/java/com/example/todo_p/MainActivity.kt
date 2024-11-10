00f2d87b065626f240d4953f01ac2fe6684440d0
PARTHA SARATHI MANNA <mannapartha2004@gmail.com>
2024-11-10 21:34:09 +0530

    add viewBinding

--- app/src/main/java/com/example/todo_p/MainActivity.kt
+++ app/src/main/java/com/example/todo_p/MainActivity.kt
@@ -10,18 +10,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.todo_p.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(binding.toolbar)
         findViewById<FloatingActionButton>(R.id.fab)?.setOnClickListener {
             startActivity(Intent(this, TaskActivity::class.java))
         }
--- app/src/main/java/com/example/todo_p/TaskActivity.kt
+++ app/src/main/java/com/example/todo_p/TaskActivity.kt
@@ -1,12 +1,28 @@
 package com.example.todo_p
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.room.Room
+import java.util.Calendar
 
+const val DB_NAME = "todo.db"
 class TaskActivity : AppCompatActivity() {
+
+    lateinit var myCalender: Calendar
+
+    lateinit var datasetListener:DatePickerDialog.OnDateSetListener
+
+    val db by lazy{
+        Room.databaseBuilder(this,
+            AppDatabase::class.java,
+            DB_NAME)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
